d98a8e68dc6fe5cd38cc7e6276bf9a282ebbbc4d
Anthony Gore <anthony@magnum.technology>
2017-11-23 12:58:10 +1100

    Converting to single page component

---
+++ resources/assets/js/components/Page.vue
@@ -0,0 +1,19 @@
+<template>
+    <h3>{{ message }}</h3>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                message: ''
+            }
+        },
+        beforeRouteEnter(to, from, next) {
+            next(vm => {
+                let name = to.name;
+                let ucfirst = name.charAt(0).toUpperCase() + name.slice(1);
+                vm.message = `Hello from the ${ucfirst} page`;
+            });
+        }
+    }
+</script>
--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -3,17 +3,15 @@ import Router from 'vue-router'
 
 Vue.use(Router);
 
-import About from './components/About.vue';
-import Contact from './components/Contact.vue';
-import Home from './components/Home.vue';
+import Page from './components/Page.vue';
 
 export function createRouter() {
   return new Router({
     mode: 'history',
     routes: [
-      { path: '/', component: Home, name: 'home' },
-      { path: '/about', component: About, name: 'about' },
-      { path: '/contact', component: Contact, name: 'contact' }
+      { path: '/', component: Page, name: 'home' },
+      { path: '/about', component: Page, name: 'about' },
+      { path: '/contact', component: Page, name: 'contact' }
     ]
   })
 }
